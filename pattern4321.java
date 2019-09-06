572a3a0229736f3c44167ed508de085ead4b93e0
Uttam Rajak <uttamkumar16jan@gmail.com>
2019-09-06 16:46:07 +0530

    v2

--- pattern4321.java
+++ pattern4321.java
@@ -4,21 +4,22 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        int n=4, m=(2*n)-1;
+        int n=4, m=(2*n)-1, o=m, p=m;
         for(int i=0;i<m;i++){
             int k=1;
             for(int j=0; j<(2*n)-1;j++){
                 System.out.print(n);
                 if(k>=(2*n)-1){
                     int temp=n;
-                    while(k<m){
+                    while(k<o){
                         System.out.print(++temp);
                         k++;
+                        o--;
                     }
                     System.out.println();
-                    while(k>=m){
+                    while(p>=k){
                         System.out.print(temp--);
-                        k--;
+                        k++;
                     }
                     break;
                 }
